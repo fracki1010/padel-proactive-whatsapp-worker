6b6ec1f360fca7bcd0b3eb2cec705879587984b4
Franco <fracki1010@gmail.com>
2026-04-21 14:13:52 -0300

    feat: agregar endpoint para obtener el número de cliente a partir del whatsappId

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+const { getNumberByUser } = require("./utils/getNumberByUser");
 const express = require("express");
 
 const app = express();
@@ -12,6 +13,17 @@ app.get("/ready", (_req, res) => {
 
 app.get("/version", (_req, res) => {
   res.status(200).json({ success: true, version: "1.0.0" });
+});
+
+//obtener el numero del cliente a partir del whatsappId (chatId)
+app.get("/get-number/:whatsappId", async (req, res) => {
+  try {
+    const whatsappId = req.params.whatsappId;
+    const phoneNumber = await getNumberByUser(whatsappId);
+    res.status(200).json({ success: true, phoneNumber });
+  } catch (error) {
+    res.status(500).json({ success: false, error: error.message });
+  }
 });
 
 const startHealthServer = () => {
